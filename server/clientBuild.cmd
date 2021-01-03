52de5b3fda6321cf2f3629079b86b47e9febd69f
shawn ning <zxning@gmail.com>
2021-01-02 22:42:51 -0500

    update

--- server/clientBuild.cmd
+++ server/clientBuild.cmd
@@ -1,7 +1,4 @@
-C:\CocosDashboard_1.0.10\resources\.editors\Creator\2.4.3\CocosCreator.exe --path ..\client\ --build "platform=web-mobile;debug=false;sourceMaps=false;md5Cache=true;encryptJs=true;autoCompile=true;embedWebDebugger=false"
+C:\CocosDashboard_1.0.10\resources\.editors\Creator\2.4.3\CocosCreator.exe --path ..\client\ --build "platform=web-mobile;debug=false;sourceMaps=false;md5Cache=true;encryptJs=true;autoCompile=true;embedWebDebugger=false;buildPath=..\public"
 rem % eascped to %% in windows batch
 ssh mj "cd easymj/public && mv web-mobile web-mobile`date +'_%%Y-%%m-%%d-%%H-%%M-%%S'`  && ls -l "
 scp -r ..\public\web-mobile\ mj:easymj/public/web-mobile/
-
-
-
